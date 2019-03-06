7fddf008ef8b8ab2500f25cb47fc627e9375e19a
Fabian Gambino <fabiangambino@localhost.localdomain>
2019-03-06 14:58:30 -0500

    Renamed contacts list to database. Added welcome, user_ui, and add_new_contact functions. Also created a cleaner program runtime.

--- fabdb.py
+++ fabdb.py
@@ -10,10 +10,12 @@ class Contact:
         print("Phone Number: " + str(self.phone_number))
         print("Email Address: " + self.email_address)
 
-#robert = Contact("Robert Smith", 7185556666, "robert@pyproj.com")
-#jason = Contact("Jason Brown", 7185557777, "jason@pyproj.com")
-#michael = Contact("Michael Freedman", 7185558888, "michael@pyproj.com")
-#eric = Contact("Eric Oswlad", 7185559999, "eric@pyproj.com")
+database = []
+
+def welcome():
+    print("\n------------------------")
+    print("Welcome to Fab Database!")
+    print("------------------------")
 
 def user_ui():
     print("\nWhat would you like to do?\n")
@@ -23,18 +25,35 @@ def user_ui():
     print("4. Search for a contact")
     print("5. Exit\n")
 
-contacts = []
+# working on a way to apply user inputs to a class object with a dynamically assigned
+# variable name relative to the name variable. Line 34 will not work because add_new_contact
+# will get overwritten with each additional new contact added by user.
+def add_new_contact():
+    name = input("\nEnter the contact's name: ")
+    phone_number = input("\nEnter the contact's phone number: ")
+    email_address = input("\nEnter the contact's email address: ")
+    new_contact = Contact(name, phone_number, email_address)
+    database.append(new_contact)
+    print("\n" + name + " was added to contacts!")
 
-#run
-print("\n------------------------")
-print("Welcome to Fab Database!")
-print("------------------------")
+#Program Runs
 
+welcome()
+
+# options 2, 3, and 4 are not built out yet
 while True:
     user_ui()
-    task = int(input("Enter the task number: "))
+    task = int(input("Enter the task number and press enter: "))
     if task == 1:
-        name = input("Enter the contact's name: ")
-        phone_number = input("Enter the contact's phone number: ")
-        email_address = input("Enter the contact's email address: ")
-        print("\n" + name + " was added to contacts!")
+        add_new_contact()
+    elif task == 2:
+        print("\nThis feature is not built yet.")
+    elif task == 3:
+        print("\nThis feature is not built yet.")
+    elif task == 4:
+        print("\nThis feature is not built yet.")
+    elif task == 5:
+        print("\n-------------------")
+        print("Exited Fab Database")
+        print("-------------------\n")
+        break
