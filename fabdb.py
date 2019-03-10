6eddcbf5011271112c6af5fb95e5631c31cc4652
Fabian Gambino <fgambinoC@gmail.com>
2019-03-10 01:25:05 -0500

    Added csv_contacts function. Also added input error checking for user input when selecting from the main menu.

--- fabdb.py
+++ fabdb.py
@@ -1,3 +1,4 @@
+# contacts class object layout
 class Contact:
 
     def __init__(self, name, phone_number, email_address):
@@ -10,13 +11,16 @@ class Contact:
         print("Phone Number: " + str(self.phone_number))
         print("Email Address: " + self.email_address)
 
+# database of contacts
 database = []
 
+# welcome user interface
 def welcome():
     print("\n------------------------")
     print("Welcome to Fab Database!")
     print("------------------------")
 
+# user interface display
 def user_ui():
     print("\nWhat would you like to do?\n")
     print("1. Add a new contact")
@@ -25,6 +29,7 @@ def user_ui():
     print("4. Search for a contact")
     print("5. Exit\n")
 
+# add new contacts functionality
 def add_new_contact():
     name = input("\nEnter the contact's name: ")
     phone_number = input("\nEnter the contact's phone number: ")
@@ -34,7 +39,14 @@ def add_new_contact():
     print("\n-------------------------------------------------")
     print(name + " was added to contacts!")
     print("-------------------------------------------------")
-    
+
+# comma separated contacts added to database
+def csv_contacts(database):
+    csv_contacts = ""
+    for contact in database:
+        csv_contacts = csv_contacts + str(contact.name) + "," + str(contact.phone_number) + "," + str(contact.email_address) + "\n"
+    return csv_contacts
+
 #Program Runs
 
 welcome()
@@ -42,25 +54,29 @@ welcome()
 # options 2, 3, and 4 are not built out yet
 while True:
     user_ui()
-    task = int(input("Enter the task number and press enter: "))
-    if task == 1:
+    task = input("Enter the task number and press enter: ")
+    if task == "1":
         add_new_contact()
-    elif task == 2:
+    elif task == "2":
         print("\n------------------------------")
         print("This feature is not built yet.")
         print("------------------------------")
-    elif task == 3:
+    elif task == "3":
         print("\n------------------------------")
         print("This feature is not built yet.")
         print("------------------------------")
-    elif task == 4:
+    elif task == "4":
         print("\n------------------------------")
         print("This feature is not built yet.")
         print("------------------------------")
-    elif task == 5:
+    elif task == "5":
         print("\n-------------------")
         print("Exited Fab Database")
         print("-------------------\n")
         break
+    else:
+        print("\n---------------------------------")
+        print("Error! Please enter numbers only.")
+        print("---------------------------------")
 
-print(database)
+csv_contacts(database)
